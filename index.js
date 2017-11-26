df49bd02921d5a5d53145ac86bf0b97ba32720d9
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-11-26 07:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) {
     return "I can't hear you!"
   }
+  if (string == string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string == string.toUpperCase()) {
+    return "I love you, too."
+  }
 }
